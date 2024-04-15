5f83f183efefd4ac91d7487609c28aca265f6739
lorenzoalessi <alessilorenzo02@gmail.com>
2024-04-15 21:20:57 +0200

    Project setup. Created two modules: web (Angular and Typescript) and api

---
+++ driver-roulette-api/src/main/java/it/lorenzoalessi/driverroulette/DriverRouletteApplication.java
@@ -0,0 +1,13 @@
+package it.lorenzoalessi.driverroulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverRouletteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverRouletteApplication.class, args);
+    }
+
+}
