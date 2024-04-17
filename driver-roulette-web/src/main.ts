0a6709020c2a246f9934348eca910e6f1260d4ff
lorenzoalessi <alessilorenzo02@gmail.com>
2024-04-17 02:51:08 +0200

    Added todo. To make the project versatile (default names and images to be made dynamic)

--- driver-roulette-web/src/main.ts
+++ driver-roulette-web/src/main.ts
@@ -2,5 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// TODO: To make the project versatile (default names and images to be made dynamic)
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
